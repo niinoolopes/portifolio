0f71c2b2f8b5d22cdafe5a0ae12ff5dafa823138
Anderson Lopes <niinoolopes0@gmail.com>
2021-12-28 14:42:19 -0300

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-  ? '/portifolio/p-vue2-tarefas'
+  ? '/portifolio/p-vue2-localStorage-todo'
   : '/',
   lintOnSave: false
 }
