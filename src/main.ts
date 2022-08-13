455bcfc55e10309aef92ed7b76873bee04917a5c
niinoolopes <niinoolopes0@gmail.com>
2022-08-13 14:40:13 -0300

    add bootstrap and fontawesome

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,21 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
+import './assets/css/app.scss'
 
+// Pinia
 import { createPinia } from 'pinia'
-
 const pinia = createPinia()
 
-createApp(App).use(pinia).use(store).use(router).mount('#app')
+// fontawesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faUtensils, faClipboardList } from '@fortawesome/free-solid-svg-icons'
+library.add(faUtensils, faClipboardList)
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(pinia)
+app.use(store)
+app.use(router)
+app.mount('#app')
